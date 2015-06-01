27d4ffbdb9849c36b75a286f6f23656bb3e7a8ef
Terrence Chun <docterry@hotmail.com>
2015-06-01 00:16:30 -0700

    skeleton for AddUser routine

--- back.php
+++ back.php
@@ -9,14 +9,14 @@
     <meta name="apple-mobile-web-app-status-bar-style" content="default" />
     <meta name="viewport" content="minimum-scale=1.0, width=device-width, maximum-scale=0.6667, user-scalable=no" />
 <!--    Block for CDN copies of jquery/mobile. Consider fallback code on fail? -->
-    <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.css" />
-    <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
-    <script src="http://code.jquery.com/mobile/1.4.2/jquery.mobile-1.4.2.min.js"></script>
+    <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
+    <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+    <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
 <!--==========================================-->
 <!-- Block for local copies of jquery/mobile. 
-    <link rel="stylesheet" href="./jqm/jquery.mobile-1.3.2.min.css" />
-    <script src="./jqm/jquery-1.9.1.min.js"></script>
-    <script src="./jqm/jquery.mobile-1.3.2.min.js"></script>
+    <link rel="stylesheet" href="./jqm/jquery.mobile-1.4.5.min.css" />
+    <script src="./jqm/jquery-1.11.1.min.js"></script>
+    <script src="./jqm/jquery.mobile-1.4.5.min.js"></script>
 <!--==========================================-->
     <!--<script type="text/javascript" src="./jqm/jqm-alertbox.min.js"></script>-->
     <script>
@@ -40,7 +40,7 @@
         });  
     </script>
     
-    <title>Paging v2</title>
+    <title>Paging v3</title>
 </head>
 <body>
 <?php
@@ -109,40 +109,57 @@ if (($handle = fopen("list.csv", "r")) !== FALSE) {
 ?>
 
 <!-- Start of first page -->
-<div data-role="page" id="main">
+<div data-role="page" id="main" >
 
-    <div data-role="header">
+<div data-role="header">
         <h4 style="white-space: normal; text-align: center" >Back End '<?php echo $userG;?>'</h4>
     </div><!-- /header -->
 
-    <ul data-role="listview">
-        <li><a href="?group=CARDS&#proc" >Cardiologists</a></li>
-        <li><a href="?group=FELLOWS" >Fellows</a></li>
-        <li><a href="?group=SURG" >CV Surgery/Anesthesia/Perfusion</a></li>
-        <li><a href="?group=CICU" >CICU</a></li>
-        <li><a href="?group=ARNP" >ARNP's</a></li>
-        <li><a href="?group=CATH" >Cath Lab</a></li>
-        <li><a href="?group=CLINIC" >Clinic RN, Social Work, Nutrition</a></li>
-        <li><a href="?group=ECHO" >Echo Lab</a></li>
-        <li><a href="?group=ADMIN" >Admin Office</a></li>
-        <li><a href="?group=DATA" >Research, Data, Computers</a></li>
-    </ul>
-    
-    <form name="mainlist" action="#proc" method="get">
-        <input type="submit" class="ui-btn ui-shadow ui-btn-icon-left ui-corner-all ui-icon-carat-l ui-btn-icon-notext" value="Cardiology">
-    </form>
+<div data-role="content">
+    <a href="#addUser" data-rel="popup" data-position-to="window" data-transition="pop" class="ui-btn ui-icon-plus ui-btn-icon-left">Add a user</a>
+    <a href="#editUser" class="ui-btn ui-icon-user ui-btn-icon-left">Edit a user</a>
+</div>
 
-    <div data-alertbox-close-time="5000" data-alertbox-transition="fade" data-role="popup" data-theme="a" data-overlay-theme="b" id="popupOpts" class="ui-content jqm-alert-box" style="max-width:280px">
-        <a href="#" data-rel="back" data-role="button" data-theme="a" data-icon="delete" data-iconpos="notext" class="ui-btn-right">Close</a>
-        <p>REMINDER!</p>
-        <p>This paging site is for internal Heart Center use. Please do not share this link with others outside of the organization. Thanks for your understanding!</p>
-    </div>
-
-    <div data-role="footer" >
+<div data-role="footer" >
         <h5><small>
-&COPY;(2007-2014) Terrence Chun, MD<br>
+&COPY;(2007-2015) Terrence Chun, MD<br>
         </small></h5>
     </div><!-- /footer -->
+
+<!--Add a new user-->
+<div data-role="popup" id="addUser" style="padding:10px 20px;">
+    <p>ADD USER</p>
+    <form method="post" action="#?action=add" data-ajax="false">
+        <div class="ui-grid-a">
+            <div class="ui-block-a">
+                <input name="nameF" id="addNameF" value="" placeholder="First name" type="text">
+            </div>
+            <div class="ui-block-b">
+                <input name="nameL" id="addNameL" value="" placeholder="Last name" type="text">
+            </div>
+        </div>
+        <input name="numPager" id="addPagerNum" value="" placeholder="Pager (10-digits)" pattern="(206)[0-9]{7}" type="text">
+        <input name="numSms" id="addSmsNum" value="" placeholder="SMS (10-digits)" pattern="[0-9]{10}" type="text">
+        <input name="numPushBul" id="addPushBul" value="" placeholder="Pushbullet email" type="text">
+        <select name="userGroup" id="addGroup" data-native-menu="false">
+            <option>Choose group</option>
+            <option value="CARDS">Cardiologists</option>
+            <option value="FELLOWS">Fellows</option>
+            <option value="SURG">CV Surgery</option>
+            <option value="CICU">Cardiac ICU</option>
+            <option value="MLP">Mid-Level Providers</option>
+            <option value="CATH">Cath Lab</option>
+            <option value="CLINIC">Clinic, Soc Work, Nutrition</option>
+            <option value="ECHO">Echo Lab</option>
+            <option value="ADMIN">Administration</option>
+            <option value="DATA">Data & Research</option>
+        </select>
+
+        <input type="hidden" name="add" value="user">
+        <button type="submit" class="ui-btn ui-corner-all ui-shadow ui-btn-b ui-btn-icon-left ui-icon-check" >Save</button>
+    </form>
+</div>
+
 </div><!-- /page -->
 
 <!-- Start of process page -->
