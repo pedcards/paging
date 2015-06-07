2e165f1132963e6b4eafb3ade4f789b8228ff352
Terrence Chun <docterry@hotmail.com>
2015-06-07 16:05:39 -0700

    starting to write out USER info

--- back.php
+++ back.php
@@ -45,6 +45,11 @@
 <body>
 <?php
 $xml = simplexml_load_file("list.xml");
+if (!($xml->groups)) {
+    $xml->addChild('groups');
+    $xml->asXML("list.xml");
+}
+$groups = $xml->groups;
 $user = $xml->xpath("//users/user");
 $userG = $user[1]->group;
 
@@ -72,21 +77,40 @@ if ($add=="user") {
     $nameL = \filter_input(\INPUT_POST, 'nameL');
     $nameF = \filter_input(\INPUT_POST, 'nameF');
     $numPager = \filter_input(\INPUT_POST, 'numPager');
+    $numPagerSys = \filter_input(\INPUT_POST, 'numPagerSys');
     $numSms = \filter_input(\INPUT_POST, 'numSms');
     $numPushBul = \filter_input(\INPUT_POST, 'numPushBul');
     $userGroup = \filter_input(\INPUT_POST, 'userGroup');
-    if ($userGroup=="Choose group") {
-        $userGroup = "";
+    if ($nameF=="" or $nameL=="") {
+        $err = "Full name required<br>";
     }
-    if ($nameF="" or $nameL="") {
-        echo '<script type="text/javascript" language="JavaScript">';
-        echo '    $(":jqmData(role="page"):last").on("pageshow", function(event) {';
-        echo '    $("#addUser", $(this)).popup("open");';
-        echo '    });';
-        echo '</script>';
+    if ($numPager=="") {
+        $err .= "Pager number required<br>";
+    }
+    if ($userGroup=="Choose group") {
+        $err .= "Group required<br>";
+    }
+    if (!$err) {                                            // No errors, write 
+        if (!($groups->$userGroup)) {
+            $groups->addChild($userGroup);
+            $xml->asXML("list.xml");
+        }
+        $groupThis = $groups->$userGroup;
+        if (!($groupThis->xpath("user[@name='".$nameL."']"))) {
+            $user = $groupThis->addChild('user');
+            $user['name'] = $nameL;
+            $user['first'] = $nameF;
+            $xml->asXML("list.xml");
+        }
+        $user = $groupThis->xpath("user[@name='".$nameL."']");
+        if ($numPager) {
+            $user[0]->addChild('pager');
+            $user[0]->pager->addChild('sys',$numPagerSys);
+        }
+        $xml->saveXML();
     }
 }
-$group = filter_input(INPUT_GET,'group');
+
 $groupfull = array(
     'CARDS' => 'Cardiologists',
     'FELLOWS' => 'Fellows',
@@ -131,24 +155,24 @@ if (($handle = fopen("list.csv", "r")) !== FALSE) {
 <div data-role="page" id="main" >
 
 <div data-role="header">
-        <h4 style="white-space: normal; text-align: center" >Back End</h4>
+        <h4 style="white-space: normal; text-align: center" >User Manager</h4>
     </div><!-- /header -->
 
 <div data-role="content">
     <?php
-    if ($add) { ?>
-    <ul data-role="listview" data-inset="true">
-        <li><a href="">First: <?php echo $nameF;?></a></li>
-        <li><a href="">Last: <?php echo $nameL;?></a></li>
-        <li><a href="">Group: <?php echo $userGroup;?></a></li>
-        <li><a href="">Pager: <?php echo $numPager;?></a></li>
-        <li><a href="">SMS: <?php echo $numSms;?></a></li>
-        <li><a href="">PushBullet: <?php echo $numPushBul;?></a></li>
-    </ul> <?php
+    if ($err) { ?>
+    <div class="ui-grid-b">
+        <div class="ui-block-a"></div>
+        <div class="ui-block-b">
+            <a href="#addUser" data-rel="popup" data-position-to="window" data-transition="pop" class="ui-btn ui-btn-b ui-corner-all">
+                <?php echo "ERROR!<br><br>" . $err . "<br>[click to try again]<br>";?>
+            </a>
+        </div>
+    </div> <?php
     }
     ?>
     <a href="#addUser" data-rel="popup" data-position-to="window" data-transition="pop" class="ui-btn ui-icon-plus ui-btn-icon-left">Add a user</a>
-    <a href="#editUser" class="ui-btn ui-icon-user ui-btn-icon-left">Edit a user</a>
+    <a href="#editUser" class="ui-btn ui-icon-edit ui-btn-icon-left">Edit a user</a>
 </div>
 
 <div data-role="footer" >
@@ -159,7 +183,7 @@ if (($handle = fopen("list.csv", "r")) !== FALSE) {
 
 <!--Add a new user-->
 <div data-role="popup" id="addUser" style="padding:10px 20px;">
-    <p>ADD USER</p>
+    <p style="text-align: center">ADD USER</p>
     <form method="post" action="#" data-ajax="false">
         <div class="ui-grid-a">
             <div class="ui-block-a">
@@ -170,6 +194,13 @@ if (($handle = fopen("list.csv", "r")) !== FALSE) {
             </div>
         </div>
         <input name="numPager" id="addPagerNum" value="<?php echo $numPager;?>" placeholder="Pager (10-digits)" pattern="(206)[0-9]{7}" type="text">
+        <fieldset data-role="controlgroup" data-type="horizontal">
+            <input name="numPagerSys" id="addPagerSys-a" type="radio">
+            <label for="addPagerSys-a">Cook Paging</label>
+            <input name="numPagerSys" id="addPagerSys-b" type="radio">
+            <label for="addPagerSys-b">USA Mobility</label>
+        </fieldset>
+        
         <input name="numSms" id="addSmsNum" value="<?php echo $numSms;?>" placeholder="SMS (10-digits)" pattern="[0-9]{10}" type="text">
         <input name="numPushBul" id="addPushBul" value="<?php echo $numPushBul;?>" placeholder="Pushbullet email" type="text">
         <select name="userGroup" id="addGroup" data-native-menu="false">
@@ -185,7 +216,6 @@ if (($handle = fopen("list.csv", "r")) !== FALSE) {
             <option value="ADMIN">Administration</option>
             <option value="DATA">Data & Research</option>
         </select>
-
         <input type="hidden" name="add" value="user">
         <button type="submit" class="ui-btn ui-corner-all ui-shadow ui-btn-b ui-btn-icon-left ui-icon-check" >Save</button>
     </form>
