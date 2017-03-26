17b2266e0dd3101347908ed0e8edbe21bec63225
Terry Chun <docterry@hotmail.com>
2017-03-25 20:36:38 -0700

    Start code for change.php

--- change.php
+++ change.php
@@ -1,8 +1,31 @@
+<!DOCTYPE html>
+<HTML>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-capable" content="no" />
+        <meta name="viewport" content="initial-scale=1, width=device-width, user-scalable=no" />
+        <?php
+        $isLoc = true;
+        $ini = parse_ini_file("paging.ini");
+        $cdnJqm = $ini['jqm'];
+        $cdnJQ = $ini['jquery'];
+        $instr = $ini['copyright'];
+        ?>
+        <link rel="stylesheet" href="<?php echo (($isLoc) ? './jqm' : 'http://code.jquery.com/mobile/'.$cdnJqm).'/jquery.mobile-'.$cdnJqm;?>.min.css" />
+        <script src="<?php echo (($isLoc) ? './jqm/' : 'http://code.jquery.com/').'jquery-'.$cdnJQ;?>.min.js"></script>
+        <script src="<?php echo (($isLoc) ? './jqm' : 'http://code.jquery.com/mobile/'.$cdnJqm).'/jquery.mobile-'.$cdnJqm;?>.min.js"></script>
+        <script src="./lib/cookies.js"></script>
+<!--==========================================-->
+        <title>Heart Center Paging</title>
+    </head>
+<body>
+
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
+/*  This section at end for committing to list.xml
+ * 
+ */
+$xml = (simplexml_load_file("list.xml")) ?: new SimpleXMLElement("<root />");
+$groups = ($xml->groups) ?: $xml->addChild('groups');
