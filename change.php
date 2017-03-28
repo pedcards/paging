efe9280addff7ef62534d7d7e12e2d704dfb4123
Terry Chun <docterry@hotmail.com>
2017-03-28 13:24:26 -0700

    function compare() returns string, pushes to $changes array

--- change.php
+++ change.php
@@ -42,9 +42,14 @@
         return trim(mcrypt_decrypt(MCRYPT_BLOWFISH, $salt, base64_decode($text), MCRYPT_MODE_ECB, mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_BLOWFISH, MCRYPT_MODE_ECB), MCRYPT_RAND)));
     }
     function compare($field,$old,$new) {
+        global $changes;
         if ($old==$new) {
             return '';
         } else {
+            array_push(
+                $changes,
+                array($field=>$new)
+            );
             return $field.": '".$old."' -> '".$new."'\r\n";
         }
     }
